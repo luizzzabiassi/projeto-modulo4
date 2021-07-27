e28bdc909104777347a146b6173cf84917b1c6f0
Luiza Biassi <luiza.biassi@gmail.com>
2021-07-27 15:35:03 -0300

    create model

---
+++ src/models/petisco-model.js
@@ -0,0 +1,8 @@
+module.exports = class Petisco {
+    
+    constructor(nome, descricao_prato, preco){
+        this.nome = nome,
+        this.descricao_prato = descricao_prato,
+        this.preco = preco
+    }
+}
