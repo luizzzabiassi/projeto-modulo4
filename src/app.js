0de63657d7789a87f2e3a50e304ba733f6040896
Luiza Biassi <luiza.biassi@gmail.com>
2021-07-27 22:25:51 -0300

    criando servidor

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+const express = require('express')
+const app = express()
+
+const rotasPetisco = require('./controllers/petisco-controller')
+const Petisco = require('./models/petisco-model')
+const db = require('./infra/db-sqlite')
+
+app.use(express.json())
+
+rotasPetisco(app, db)
+
 module.exports = app
--- src/server.js
+++ src/server.js
@@ -1,1 +1,6 @@
 const app = require('./app')
+const port = 3050
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Servidor rodando: http://localhost:${process.env.PORT || port}`);
+})
